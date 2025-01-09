d00af41d6ab9214c053676bca49324b98dcdb2d4
neolooong <j31422210@gmail.com>
2025-01-09 21:53:59 +0800

    fix: don't search when plugin loaded

--- lua/whichpy/init.lua
+++ lua/whichpy/init.lua
@@ -5,7 +5,6 @@ M.locators = {}
 ---@param opts WhichPy.Config
 M.setup = function(opts)
   require("whichpy.config").setup_config(opts or {})
-  require("whichpy.envs").asearch()
   require("whichpy.usercmd").create_user_cmd()
   require("whichpy.lsp").create_autocmd()
   require("whichpy.envs").retrieve_cache()
