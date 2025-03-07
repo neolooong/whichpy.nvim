523f73112202d6d0999927799a34e6c9b8d49e31
neolooong <j31422210@gmail.com>
2025-03-07 20:55:26 +0800

    feat: improve reset handling (pyright)

--- lua/whichpy/lsp/handlers/pyright.lua
+++ lua/whichpy/lsp/handlers/pyright.lua
@@ -1,3 +1,9 @@
+-- FIXME: may cause bug if pyright and basedpyright enable at the same time
+
+local is_pyright_python_check = false
+local pyright_python_default = nil
+local pyright_python_origin = nil
+
 ---@type WhichPy.Lsp.PyrightHandler
 local M = {
   get_python_path = function(client)
@@ -7,6 +13,17 @@ local M = {
     return nil
   end,
   set_python_path = function(client, python_path)
+    if not is_pyright_python_check then
+      is_pyright_python_check = true
+      if client.settings.python then
+        pyright_python_origin = client.settings.python.pythonPath
+      else
+        pyright_python_origin = pyright_python_default
+      end
+    end
+
+    python_path = python_path or pyright_python_origin
+
     if python_path then
       client.settings =
         vim.tbl_deep_extend("force", client.settings, { python = { pythonPath = python_path } })
