276d7811611ad9c59f37f27f1af831abd31cf7fb
George Harker <george@george-graphics.co.uk>
2025-03-10 11:52:24 -0700

    Delay cache retrieval until VimEnter is done

--- lua/whichpy/init.lua
+++ lua/whichpy/init.lua
@@ -5,7 +5,14 @@ M.setup = function(opts)
   require("whichpy.config").setup_config(opts or {})
   require("whichpy.usercmd").create_user_cmd()
   require("whichpy.lsp").create_autocmd()
-  require("whichpy.envs").retrieve_cache()
+
+  M._augroup = vim.api.nvim_create_augroup("WhichPyStartup", { clear = true })
+  vim.api.nvim_create_autocmd({ "VimEnter" }, {
+    group = M._augroup,
+    callback = function()
+      require("whichpy.envs").retrieve_cache()
+    end
+  })
 end
 
 return M
