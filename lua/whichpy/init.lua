e8d32de50e7805b9d574c638e832608cf1959668
George Harker <george@george-graphics.co.uk>
2025-03-13 08:56:35 -0700

    Resolve cache if lazy loaded

--- lua/whichpy/init.lua
+++ lua/whichpy/init.lua
@@ -6,13 +6,17 @@ M.setup = function(opts)
   require("whichpy.usercmd").create_user_cmd()
   require("whichpy.lsp").create_autocmd()
 
-  M._augroup = vim.api.nvim_create_augroup("WhichPyStartup", { clear = true })
-  vim.api.nvim_create_autocmd({ "VimEnter" }, {
-    group = M._augroup,
-    callback = function()
-      require("whichpy.envs").retrieve_cache()
-    end
-  })
+  if vim.v.vim_did_enter == 0 then
+    M._augroup = vim.api.nvim_create_augroup("WhichPyStartup", { clear = true })
+    vim.api.nvim_create_autocmd({ "VimEnter" }, {
+      group = M._augroup,
+      callback = function()
+        require("whichpy.envs").retrieve_cache()
+      end
+    })
+  else
+    require("whichpy.envs").retrieve_cache()
+  end
 end
 
 return M
