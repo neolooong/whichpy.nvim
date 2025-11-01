7583d64237367a06d3cca976f188ec79be0c2f3b
George Harker <george@george-graphics.co.uk>
2025-11-01 14:44:46 -0700

    modernize setup

--- lua/whichpy/init.lua
+++ lua/whichpy/init.lua
@@ -2,23 +2,22 @@ local M = {}
 
 ---@param opts WhichPy.Config
 M.setup = function(opts)
-  local config = require("whichpy.config")
-  config.setup_config(opts or {})
+  require("whichpy.config").setup_config(opts or {})
   require("whichpy.usercmd").create_user_cmd()
   require("whichpy.lsp").create_autocmd()
 
-  if config.config.set_env_on_startup then
-    if vim.v.vim_did_enter == 0 then
-      M._augroup = vim.api.nvim_create_augroup("WhichPyStartup", { clear = true })
-      vim.api.nvim_create_autocmd({ "VimEnter" }, {
-        group = M._augroup,
-        callback = function()
+  -- Commands that emit output should wait until startup is complete
+  -- to ensure notifications etc are set up.
+  if vim.v.vim_did_enter == 0 then
+    M._augroup = vim.api.nvim_create_augroup("WhichPyStartup", { clear = true })
+    vim.api.nvim_create_autocmd({ "VimEnter" }, {
+      group = M._augroup,
+      callback = function()
         require("whichpy.envs").retrieve_cache()
-        end
-      })
-    else
-      require("whichpy.envs").retrieve_cache()
-    end
+      end,
+    })
+  else
+    require("whichpy.envs").retrieve_cache()
   end
 end
 
